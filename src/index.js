03e572e4d8572e667f7e5107ffe19012d8341c5b
TroitskiyAndrew <TroitskiyA@yandex.ru>
2021-09-03 23:47:02 +0500

    Complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,31 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let binLetters = [];
+    let morseLetters = [];
+    for (let i = 0; i < expr.length; i += 10){
+        binLetters.push(expr.substring(i, i+10))
+    }
+    for (let letterbin of binLetters) {
+        let letterMorse = ""
+        for (i = 0; i < 10; i += 2){
+            let bin =  letterbin.substr(i, 2);
+            if (bin === "**") {
+                letterMorse += " ";
+                break;
+            } else if (bin === "00"){
+                continue
+            } else if (bin === "10"){
+                letterMorse += ".";
+            }else if (bin === "11"){
+                letterMorse += "-";
+            }
+        }
+        morseLetters.push(letterMorse);
+    }
+    return morseLetters.map((mors) => {
+        return mors === " " ? mors : MORSE_TABLE[mors]
+    }).join("");
 }
 
 module.exports = {
